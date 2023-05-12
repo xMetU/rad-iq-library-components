0ed2aad986f37231b758c1cdf1a81931eb7b533f
Matthew Owen <matthew.owen@y7mail.com>
2023-05-12 20:09:42 +0930

    made database changes without breaking installer

--- com_myImageViewer/admin/sql/install.mysql.utf8.sql
+++ com_myImageViewer/admin/sql/install.mysql.utf8.sql
@@ -3,18 +3,16 @@ DROP TABLE IF EXISTS `#__myImageViewer_imageCategory`;
 
 CREATE TABLE IF NOT EXISTS `#__myImageViewer_imageCategory` (
   `id` SERIAL NOT NULL,
-  `categoryName` VARCHAR(45)  NOT NULL,
+  `categoryName` VARCHAR(30) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `#__myImageViewer_image` (
   `id` SERIAL NOT NULL,
-  `imageName` VARCHAR(50)  NOT NULL,
-  `categoryId` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
-  `imageDescription` TEXT,
-  `imageProjection` VARCHAR(200),
+  `imageName` VARCHAR(60) NOT NULL,
+  `categoryId` bigint(10) UNSIGNED NOT NULL DEFAULT '0',
+  `imageDescription` VARCHAR(12000),
   `imageUrl` VARCHAR(200) NOT NULL,
-  `imageCitation` VARCHAR(200),
   PRIMARY KEY (`id`),
   FOREIGN KEY (`categoryId`) REFERENCES `#__myImageViewer_imageCategory` (`id`)
 ) ENGINE = InnoDB;
