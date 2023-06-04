33e0a4ad139a94bee5cd284079a04634ee8aef8a
Matthew Owen <matthew.owen@y7mail.com>
2023-06-04 19:16:36 +0930

    more updates

--- com_myfrontpage/site/tmpl/FrontPageView/default.php
+++ com_myfrontpage/site/tmpl/FrontPageView/default.php
@@ -18,11 +18,9 @@ $document->addStyleSheet("media/com_myfrontpage/css/style.css");
 
 <!-- Header -->
 <div class="row">
-	<div class="col">
-		<div class="text-center">
-			<h1>Welcome to Rad IQ Library!</h1>
-			<h4>Image Repository and Learning Resource</h4>
-		</div>
+	<div class="col text-center mt-5">
+		<h1>Welcome to Med Rad IQ Library!</h1>
+		<h4>Image Repository and Learning Resource</h4>
 	</div>
 </div>
 
@@ -30,10 +28,10 @@ $document->addStyleSheet("media/com_myfrontpage/css/style.css");
 
 <!-- Content -->
 <div class="row justify-content-center">
-	<div class="col-10">
-		<div>
+	<div class="col-10 card">
+		<div class="card-body">
 			<p>
-				Welcome to Rad IQ Library, a web-based application that aims to serve as a comprehensive resource for Medical
+				Welcome to Med Rad IQ Library, a web-based application that aims to serve as a comprehensive resource for Medical
 				Imaging Students. Our library houses an extensive collection of radiographic images and content, carefully curated
 				to facilitate a deeper understanding of Medical Imaging Studies (RADY courses). By providing real-world cases and
 				images, we offer contemporary and relevant insights into the field of medical imaging.
@@ -42,7 +40,7 @@ $document->addStyleSheet("media/com_myfrontpage/css/style.css");
 			<br/>
 
 			<p>
-				At Rad IQ Library, we understand the importance of practical learning. That's why we have integrated self-assessment
+				At Med Rad IQ Library, we understand the importance of practical learning. That's why we have integrated self-assessment
 				modules and quizzes into our platform, allowing students to evaluate their progress and enhance their knowledge.
 				Whether you're a budding medical imaging professional or a student looking to embark on a career in this field, our
 				platform is here to provide you with essentail resources.
@@ -53,10 +51,11 @@ $document->addStyleSheet("media/com_myfrontpage/css/style.css");
 			<p>
 				This platform is a result of the collaboration between the STEM and Allied Health & Human Performance Academic Units
 				at the University of South Australia. We are dedicated to fostering continuous learning and encouraging exploration
-				in the field of medical imaging. Join us on this educational journey and unlock the possibilities that Rad IQ Library
+				in the field of medical imaging. Join us on this educational journey and unlock the possibilities that Med Rad IQ Library
 				has to offer.
 			</p>
 		</div>
+		
 	</div>
 </div>
 
